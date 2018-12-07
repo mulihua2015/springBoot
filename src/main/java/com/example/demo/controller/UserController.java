b7dfac7478e4ca635a763fa5a1c20b4caab56163
huangzecheng928 <283966973@qq.com>
2018-12-07 22:15:27 +0800

    update

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,32 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @RequestMapping("/delete2")
-    public String delete2(){
-        return "delete2";
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
     }
-
-    @RequestMapping("/delete3")
-    public String delete3(){
-        return "delete2";
-    }
-
-    @RequestMapping("/test")
-    public String test(){
-        return "test";
-    }
-
-
-    @RequestMapping("/delete4")
-    public String delete(){
-        return "delete4";
-    }
-
-
-    @RequestMapping("/delete5")
-    public String delete5(){
-        return "delete5";
-    }
-
 
 }
